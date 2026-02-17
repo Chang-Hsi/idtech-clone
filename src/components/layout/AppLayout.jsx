3a2667f0e1785a867b2ee242b53f0ed4a47f19ee
Chang-Hsi <watasiwa8531@gmail.com>
2026-02-17 15:46:45 +0800

    IDT-5 fix scroll to top on route change

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
+import ScrollToTopOnRouteChange from './ScrollToTopOnRouteChange'
 
 const AppLayout = () => {
   return (
     <div className="min-h-screen overflow-x-hidden bg-[#171A1F]">
+      <ScrollToTopOnRouteChange />
       <Header />
       <main className="w-full">
         <Outlet />
